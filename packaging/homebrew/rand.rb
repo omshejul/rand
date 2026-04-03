7ffa682918b3471d089607a5ed8e16db24b5c209
Om Shejul <contact@omshejul.com>
2026-04-03 15:52:36 +0530

    Update Homebrew formula template for v0.1.0

--- packaging/homebrew/rand.rb
+++ packaging/homebrew/rand.rb
@@ -2,7 +2,7 @@ class Rand < Formula
   desc "Generate random alphanumeric strings"
   homepage "https://github.com/omshejul/rand"
   url "https://github.com/omshejul/rand/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_RELEASE_TARBALL_SHA256"
+  sha256 "4733cdb1ad33da05928db23a7e5de01822098f6659cbe19c9e3fa1e0803ab5ca"
   license "MIT"
 
   def install
